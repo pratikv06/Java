d4083699c8b16e67caaa315f53bd4c1791097fad
Pratik Vishwakarma <pratikvishwakarma06.pv@gmail.com>
2020-02-07 19:21:17 +0530

    Section: Ternary Operator

---
+++ Teach Yourself Java/ControlStatement/TernaryOperatorDemo.java
@@ -0,0 +1,14 @@
+/**
+ * Program:
+ * Illustrate the use of ternary operator
+ */
+
+class TernaryOperatorDemo {
+   public static void main(String[] args) {
+      int i=10, j=5;
+      System.out.println((i>j)?i:j);
+      System.out.println((j%2==0)?"Even":"Odd");
+      System.out.println((i%2==0)?"Even":"Odd");
+
+   }
+}
